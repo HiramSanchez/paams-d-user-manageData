77e96211f789d3d8c9d26252e7623f8c4282a226
Hiram Sanchez <hiram.sanchz@gmail.com>
2024-10-09 01:52:02 -0700

    feat: add OpenApi for web swagger

--- src/main/java/com/paa/dms/user/manage/data/Main.java
+++ src/main/java/com/paa/dms/user/manage/data/Main.java
@@ -1,6 +1,5 @@
 package com.paa.dms.user.manage.data;
 
-import com.paa.dms.user.manage.data.constants.APIConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
